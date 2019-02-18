993b7cb3805f3bec5ac20f8b2ea3d6b0e4759f02
Lucas Teixeira <weblucas@gmail.com>
2019-02-18 09:34:43 +0100

    bug

--- dataloaders/visim_dataloader.py
+++ dataloaders/visim_dataloader.py
@@ -39,8 +39,10 @@ class VISIMDataset(MyDataloaderExt):
 
         for key, value in attrib_list.items():
             attrib_np[key] = transform(value)
-            if key in ['gt_depth','fd','kor','kde','kgt','dor','dde', 'd3dwde','d3dwor']:
+            if key in ['gt_depth','fd','kor','kde','kgt','dor','dde', 'd3dwde','d3dwor','dvor','dvde','dvgt']:
                 attrib_np[key] = attrib_np[key]  / self.depth_divider
+            elif key in ['d2dwor', 'd2dwde', 'd2dwgt']:
+                attrib_np[key] = attrib_np[key] / (iwidth * 1.5)  # 1.5 about sqrt(2)- square's diagonal
 
         if 'rgb' in attrib_np:
             attrib_np['rgb'] = self.color_jitter(attrib_np['rgb'])  # random color jittering
